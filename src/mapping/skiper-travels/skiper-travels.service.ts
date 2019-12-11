7db48122009b89f54c262d36ba102bc3c068fd74
Rolando Sanchez <rsanchezbaltodano@gmail.com>
2019-12-11 03:44:17 -0600

    elimino comentarios

--- src/mapping/skiper-travels/skiper-travels.service.ts
+++ src/mapping/skiper-travels/skiper-travels.service.ts
@@ -67,8 +67,7 @@ export class SkiperTravelsService {
             .andWhere("SkiperTariffs.idcity = :idcity", { idcity: citie.id })
             .andWhere("SkiperTariffs.id_skiper_cat_travels = :idcategoriaviaje", { idcategoriaviaje })
             .getMany()
-        //console.log(tarifas);
-
+        
         if (tarifas.length == 0)
             throw new HttpException(
                 "There are no rates available in this city",
