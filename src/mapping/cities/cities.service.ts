95353873b15614d932328e437b22f2a8d344693a
Rolando Sanchez <rsanchezbaltodano@gmail.com>
2019-12-13 13:30:15 -0600

    prueba

--- src/mapping/cities/cities.service.ts
+++ src/mapping/cities/cities.service.ts
@@ -9,25 +9,25 @@ export class CitiesService {
 
     constructor(
         @InjectRepository(Cities) private readonly repository: Repository<Cities>,
-        private readonly countryService:CountrieService
+        private readonly countryService: CountrieService
     ) { }
 
     async getAll(): Promise<Cities[]> {
-        return await this.repository.find({relations:["country"]});
+        return await this.repository.find({ relations: ["country"] });
     }
-
-    async getById(id:number):Promise<Cities>{
+    //prueba
+    async getById(id: number): Promise<Cities> {
         return await this.repository.findOne({
-            where:{id:id},
-            relations:["country"]
+            where: { id: id },
+            relations: ["country"]
         })
     }
 
-    async getCitiesByCountryId(idcountry:number):Promise<Cities[]>{
+    async getCitiesByCountryId(idcountry: number): Promise<Cities[]> {
         let _country = await this.countryService.getById(idcountry);
         return await this.repository.find({
-            relations:["country"],
-            where:{country:_country}
+            relations: ["country"],
+            where: { country: _country }
         })
     }
 }
