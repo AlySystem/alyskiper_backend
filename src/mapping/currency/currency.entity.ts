fecbcda41de02fbeb0d04fabb801e116e8fb9f7c
Rolando Sanchez <rsanchezbaltodano@gmail.com>
2020-01-07 14:55:55 -0600

    agrego crud de exchange rate

--- src/mapping/currency/currency.entity.ts
+++ src/mapping/currency/currency.entity.ts
@@ -1,5 +1,6 @@
 import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
 import { CountryPaymentCurrency } from '../country-payment-currency/country-payment-currency.entity';
+import { ExchangeRate } from '../exchange-rate/exchange-rate.entity';
 
 @Entity('currency')
 export class Currency {
@@ -8,5 +9,6 @@ export class Currency {
     @Column('int', { nullable: false }) idcountry: number;
     @Column('boolean') isCrypto: boolean;
 
+    @OneToMany(type => ExchangeRate, exchangerate => exchangerate.currency) exchangerate: ExchangeRate[];
     @OneToMany(type => CountryPaymentCurrency, x => x.currency) countrypaymentcurrency: CountryPaymentCurrency[];
 }
